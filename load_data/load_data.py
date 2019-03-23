d0e2c2ff961ce979ff44c30df23ac1c961d936f8
natashach <natasha@oribi.io>
2019-03-23 19:29:42 +0200

    created a main.py file to start loading the data

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -4,27 +4,24 @@ import os
 
 from PIL import Image
 
-dirname = os.path.dirname(__file__)
-_path_to_data_dir = os.path.join(dirname, '../data')
 
-
-def load_folder(path, is_train):
+def _load_folder(path, is_train):
     if is_train:
         path = os.path.join(path, "training")
     else:
         path = os.path.join(path, "test")
     images_path = os.path.join(path, "images")
-    images = load_images_in_path(images_path)
+    images = _load_images_in_path(images_path)
     masks = os.path.join(path, "mask")
-    masks = load_images_in_path(masks)
+    masks = _load_images_in_path(masks)
     if is_train:
         manual_path = os.path.join(path, "1st_manual")
-        manual = load_images_in_path(manual_path)
+        manual = _load_images_in_path(manual_path)
         return images, masks, manual
     return images, masks
 
 
-def load_images_in_path(path):
+def _load_images_in_path(path):
     x = []
     for f in listdir(path):
         if f.endswith("tif"):
@@ -35,10 +32,7 @@ def load_images_in_path(path):
 
 
 def load(path):
-    x_train, masks, manual = load_folder(path, True)
-    x_test, masks = load_folder(path, False)
+    x_train, masks, manual = _load_folder(path, True)
+    x_test, masks = _load_folder(path, False)
     print(len(x_train))
     print(len(x_test))
-
-
-load(_path_to_data_dir)
---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from load_data import load_data
+
+def main():
+    dirname = os.path.dirname(__file__)
+    _path_to_data_dir = os.path.join(dirname, 'data')
+    load_data.load(_path_to_data_dir)
+
+
+main()
