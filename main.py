673079cb4114298938314c5d0a1f5861097d40e2
natashach <natasha@oribi.io>
2019-03-23 19:44:01 +0200

    installed pytorch

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from __future__ import print_function
 import os
 from load_data import load_data
+
+import torch
 
 def main():
     dirname = os.path.dirname(__file__)
     _path_to_data_dir = os.path.join(dirname, 'data')
     load_data.load(_path_to_data_dir)
+
+
+    x = torch.rand(5, 3)
+    print(x)
 
 
 main()
