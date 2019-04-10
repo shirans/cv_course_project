fb27acf361723406ca255a1b927f1afd00b17daf
natashach <natasha@oribi.io>
2019-04-10 19:45:20 +0300

    added another conv layer to FC

--- models/fc.py
+++ models/fc.py
@@ -6,12 +6,14 @@ import torch.nn.functional as F
 class FC(nn.Module):
     def __init__(self):
         super(FC, self).__init__()
-        in_channels = 3
-        out_channels = 1
-        kernel_size = 3
-        stride = 1
-        padding = 1
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        in_channels = 3 # Number of channels in the input image
+        out_channels = 1 # Number of channels produced by the convolution
+        kernel_size = 3 # Size of the convolving kernel
+        stride = 1 # Stride of the convolution. Default: 1
+        padding = 1 # Zero-padding added to both sides of the input. Default: 0
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
 
     def forward(self, x):
-        return self.conv(x)
+        x = F.relu(self.conv1(x))
+        return self.conv2(x)
