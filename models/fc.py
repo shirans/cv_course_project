ec120db9e360f2bb2bddff1443b4bab06fc7a914
natashach <natasha@oribi.io>
2019-04-10 19:21:11 +0300

    parameters with names for convenience

--- models/fc.py
+++ models/fc.py
@@ -6,7 +6,12 @@ import torch.nn.functional as F
 class FC(nn.Module):
     def __init__(self):
         super(FC, self).__init__()
-        self.conv = nn.Conv2d(3, 1, 3, 1, 1)
+        in_channels = 3
+        out_channels = 1
+        kernel_size = 3
+        stride = 1
+        padding = 1
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
 
     def forward(self, x):
         return self.conv(x)
